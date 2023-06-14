68b7c056d2276777b316e1521acd9173fd80c27f
danielvilarcosta <danielvilar12@gmail.com>
2023-06-14 08:24:10 -0300

    EX 1 - Classificação de Idade v1.0

--- exercicio1.js
+++ exercicio1.js
@@ -6,3 +6,17 @@
  * adulto (18-59 anos) ou idoso (60 anos ou mais). Utiliza uma estrutura condicional
  * (if, else if, else).
  */
+// Atribuir um valor à variável idade
+// Atribuir um valor à variável idade
+let idade = 100;
+
+// Verificar em qual categoria a idade se enquadra
+if (idade >= 0 && idade <= 12) {
+  console.log("Criança");
+} else if (idade >= 13 && idade <= 17) {
+  console.log("Adolescente");
+} else if (idade >= 18 && idade <= 59) {
+  console.log("Adulto");
+} else {
+  console.log("Idoso");
+}
