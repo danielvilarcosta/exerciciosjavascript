4130573014bd19b02d31b50aa614860545d9d1fe
danielvilarcosta <danielvilar12@gmail.com>
2023-06-16 07:30:52 -0300

    Exercicio 2 Finalizado Swtich

--- exercicio2.js
+++ exercicio2.js
@@ -5,3 +5,20 @@
  * a tradução para a mesma cor em português. Utiliza uma estrutura condicional (switch)
  * para fazer o mapeamento das cores.
  */
+let cor = "azul";    
+switch (cor) {
+    case "red":
+        console.log("vermelho");
+        break;
+    case "blue":
+        console.log("azul");
+        break;
+    case "green":
+        console.log("verde");
+        break;
+    case "yellow":
+        console.log("amarelo");
+        break;
+    case "pink":
+        console.log("rosa");
+}
