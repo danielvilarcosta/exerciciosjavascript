a54e2a9367ed4940bc0ddfa0ae1f2a8681a6f22d
danielvilarcosta <danielvilar12@gmail.com>
2023-06-16 07:51:59 -0300

    Exercicio Verificar número primo v1.0

--- exercicio3.js
+++ exercicio3.js
@@ -5,3 +5,26 @@
  * é um número primo. Um número primo é aquele que é divisível apenas por 1 e por ele mesmo.
  * Utiliza uma estrutura de repetição (do while) para verificar as divisões.
  */
+let numero = 11;
+
+let primo = true;
+let i = 2;
+
+if (numero < 2) {
+  primo = false; // Números menores que 2 não são primos
+} else {
+  do {
+    if (numero % i === 0) {
+      primo = false; // O número é divisível por outro valor além de 1 e ele mesmo
+      break;
+    }
+    i++;
+  } while (i * i <= numero);
+}
+
+// Verifica se o número é primo e imprime o resultado
+if (primo) {
+  console.log(numero + " é um número primo.");
+} else {
+  console.log(numero + " não é um número primo.");
+}
