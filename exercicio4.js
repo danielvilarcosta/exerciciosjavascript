b0dc9378d489896a280443ae45e7828c445b22c1
danielvilarcosta <danielvilar12@gmail.com>
2023-06-19 08:56:20 -0300

    Finalizado Ex 4 - Imprimir sequência de Fibonacci

--- exercicio4.js
+++ exercicio4.js
@@ -6,3 +6,20 @@
  * 0, 1, 1, 2, 3, 5, 8, 13, ...
  * Utiliza uma estrutura de repetição (while) para gerar a sequência.
  */
+
+let numero = 100 
+
+let sequencia = [];
+let a = 0;
+let b = 1;
+
+while (a <= numero) {
+  sequencia.push(a);
+
+  let temp = a;
+  a = b;
+  b = temp + b;
+}
+
+console.log("Sequência de Fibonacci até o número " + numero + ":");
+console.log(sequencia.join(", "));
